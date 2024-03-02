d1cd2fd3a8c582b1b8fa84f5d8af216092ae5196
temportalflux <dustin.yost.t@gmail.com>
2024-03-02 10:48:08 -0500

    Add some QoL builder changes for pushing children

--- src/builder.rs
+++ src/builder.rs
@@ -96,30 +96,6 @@ impl NodeBuilder {
 		self
 	}
 
-	pub fn push_child(&mut self, node: kdl::KdlNode) {
-		self.children.push(node);
-	}
-
-	pub fn push_child_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &impl AsKdl) {
-		self.push_child(data.as_kdl().build(name))
-	}
-
-	pub fn push_child_opt(&mut self, node: kdl::KdlNode) {
-		let has_children = node.children().map(|doc| !doc.is_empty()).unwrap_or(false);
-		if !node.entries().is_empty() || has_children {
-			self.push_child(node);
-		}
-	}
-
-	pub fn push_child_opt_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &impl AsKdl) {
-		self.push_child_opt(data.as_kdl().build(name))
-	}
-
-	pub fn with_child(mut self, node: kdl::KdlNode) -> Self {
-		self.push_child(node);
-		self
-	}
-
 	pub fn push_child_entry(&mut self, name: impl Into<kdl::KdlIdentifier>, entry: impl Into<kdl::KdlEntry>) {
 		self.push_child(Self::default().with_entry(entry.into()).build(name));
 	}
@@ -138,5 +114,65 @@ impl std::ops::AddAssign for NodeBuilder {
 	fn add_assign(&mut self, mut rhs: Self) {
 		self.entries.append(&mut rhs.entries);
 		self.children.append(&mut rhs.children);
+	}
+}
+
+// Building children
+impl NodeBuilder {
+	// Pushes a node into the list of children.
+	pub fn push_child(&mut self, node: kdl::KdlNode) {
+		self.children.push(node);
+	}
+
+	// Builds a node from an `AsKdl` type, and then pushes the node into the list of children.
+	pub fn push_child_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &impl AsKdl) {
+		self.push_child(data.as_kdl().build(name))
+	}
+
+	// Pushes a node into the list of children, only if it is not empty.
+	// Empty nodes are those without arguments, properties, or children.
+	pub fn push_child_nonempty(&mut self, node: kdl::KdlNode) {
+		let has_children = node.children().map(|doc| !doc.is_empty()).unwrap_or(false);
+		if !node.entries().is_empty() || has_children {
+			self.push_child(node);
+		}
+	}
+
+	// Builds a node from an `AsKdl` type, and then pushes the node into the list of children, only if the node is not empty.
+	pub fn push_child_nonempty_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &impl AsKdl) {
+		self.push_child_nonempty(data.as_kdl().build(name))
+	}
+	
+	// Pushes an optional node into the list of children.
+	pub fn push_child_opt(&mut self, node: Option<kdl::KdlNode>) {
+		if let Some(node) = node {
+			self.push_child(node);
+		}
+	}
+
+	// Builds a node from an `AsKdl` type (if non-none), and then pushes the node into the list of children.
+	pub fn push_child_opt_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &Option<impl AsKdl>) {
+		self.push_child_opt(data.as_ref().map(|data| data.as_kdl().build(name)))
+	}
+
+	pub fn push_child_opt_nonempty(&mut self, node: Option<kdl::KdlNode>) {
+		if let Some(node) = node {
+			self.push_child_nonempty(node)
+		}
+	}
+
+	pub fn push_child_opt_nonempty_t(&mut self, name: impl Into<kdl::KdlIdentifier>, data: &Option<impl AsKdl>) {
+		self.push_child_opt_nonempty(data.as_ref().map(|data| data.as_kdl().build(name)))
+	}
+
+	pub fn with_child(mut self, node: kdl::KdlNode) -> Self {
+		self.push_child(node);
+		self
+	}
+
+	pub fn push_children_t<'this, 'iter, 'item, Iter, Item>(&'this mut self, name: impl Into<kdl::KdlIdentifier> + Clone, iter: Iter) where Iter: Iterator<Item=&'item Item>, Item: AsKdl + 'item {
+		for data in iter {
+			self.push_child_t(name.clone(), data);
+		}
 	}
 }
